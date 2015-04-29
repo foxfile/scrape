fb67d2af43a0780b8e89cda755b5860157f69aa0
Steven Black <steveb@stevenblack.com>
2015-04-29 17:22:30 -0400

    Now collecting more data from the HTML.

--- scrape.rb
+++ scrape.rb
@@ -9,8 +9,8 @@ class Article
   attr_accessor :title
   attr_accessor :folder
   attr_accessor :description
-  attr_accessor :article_url
-  attr_accessor :samples_url
+  attr_accessor :pdf_url
+  attr_accessor :code_url
 end
 
 if !File.exist?('papers.html')
@@ -25,8 +25,23 @@ articles = Array.new
 
 page.css( "h3" ).each { |tag|
 	art = Hash.new
-	art[:title]  = tag.text
-	art[:folder] = tag.text.gsub(/\?|\!|\'|\:|\,|\.0/,"").gsub(/\s/,"_")
+  art_title = tag.text
+  next if art_title == "My personal Web site"
+	art[:title]  = art_title
+	art[:folder] = art_title.gsub(/\?|\!|\'|\:|\,|\.0/,"").gsub(/\s/,"_")
+
+  desc = tag.next_element
+  art[:description] = desc.text
+
+  linkspara = desc.next_element
+  links = linkspara.css( "a" )
+  if links.length > 0
+    art[:pdf_url] = links[0]["href"]
+    if links.length > 1
+      art[:samples_url] = links[1]["href"]
+    end
+  end
+
 	articles << art
 }
 
